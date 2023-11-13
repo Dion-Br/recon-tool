6a7fc71a0abbed3c42b0ac61681da1407168ea95
Dion Brovina <s129267@ap.be>
2023-11-13 23:36:08 +0100

    port scanner geimplementeerd

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from icmp import send_icmp_request
+from portscan import check_port
 
 # Volledige recon tool
 def main():
-    if send_icmp_request(target="99.99.99.99") is not False:
+    target = "www.google.com"
+    if send_icmp_request(target) is not False:
         # Als target online is verder gaan met recon
-        print("Hello world")
-    
+        # Port scanner implementeren
+        check_port(target, 80)
+        check_port(target, 443)
+        check_port(target, 8000)
+        check_port(target, 22)
+        
 
 if __name__ == "__main__":
     main()
